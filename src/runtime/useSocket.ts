ba2730c1ae837317c6f4fb66ed5b142f9fd82a1a
luiacks <luiackspider@gmail.com>
2025-03-25 05:45:32 +0900

    Fix: add types

--- src/runtime/useSocket.ts
+++ src/runtime/useSocket.ts
@@ -1,8 +1,10 @@
 import { onUnmounted, ref } from "vue";
 import { io } from "socket.io-client";
+import type { Ref } from "vue";
+import type { Socket } from "socket.io-client";
 
-export function useSocket(...args: Parameters<typeof io>) {
-  const socket = ref(io(...args));
+export function useSocket(...args: Parameters<typeof io>): Ref<Socket> {
+  const socket = ref(io(...args)) as Ref<Socket>;
 
   onUnmounted(() => {
     socket.value.disconnect();
